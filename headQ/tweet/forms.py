262310d77a1b76ede688b587f76eef479d8e46d2
Amalsha Fernando <it20055158@my.sliit.lk>
2024-07-06 00:09:59 +0530

    made logins, improved CRUD

--- headQ/tweet/forms.py
+++ headQ/tweet/forms.py
@@ -1,11 +1,15 @@
 from django import forms
 from .models import Tweet
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
-        fields = ['text', 'photo']
-'''
+        fields = ['text', 'photo'] #array is used coz here we create new form
+
 class UserRegistrationForm(UserCreationForm):
-    '''
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2') #tuple is used coz we here use built-in form
